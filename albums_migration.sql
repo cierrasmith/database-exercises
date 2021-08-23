784593e12fa39eaad40c12f5905ba64c03eafe2e
Cierra Smith <cierra.r.smith3@gmail.com>
2021-08-23 11:13:08 -0500

    updated completed tables exercise

--- albums_migration.sql
+++ albums_migration.sql
@@ -4,10 +4,12 @@ drop table if exists albums;
 
 CREATE TABLE albums (
                         id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-                        artist VARCHAR(50) DEFAULT 'NONE',
-                        name  VARCHAR(100) NOT NULL,
+                        artist VARCHAR(100) NOT NULL,
+                        name  VARCHAR(200) NOT NULL,
                         release_date YEAR,
-                        sales FLOAT DOUBLE,
-                        genre VARCHAR(50),
+                        sales FLOAT,
+                        genre VARCHAR(100),
                         PRIMARY KEY (id)
 );
+
+describe albums;
