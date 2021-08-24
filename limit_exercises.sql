b2c6a30ed9213db80cf09151220f14f6698996ba
Cierra Smith <cierra.r.smith3@gmail.com>
2021-08-24 15:03:55 -0500

    added alias

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -1,7 +1,7 @@
 USE employees;
 
 # List the first 10 distinct last name sorted in descending order. Your result should look like this:
-SELECT DISTINCT last_name FROM employees ORDER BY last_name DESC limit 10;
+SELECT DISTINCT e.last_name FROM employees.employees e ORDER BY e.last_name DESC limit 10;
 
 # Create a query to get the top 5 salaries and display just the employees number from the salaries table
 SELECT emp_no FROM salaries ORDER BY salary DESC limit 5;
