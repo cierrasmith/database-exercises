bae965e4b7fb0fc8b1cb1242c61e82141a352a73
Cierra Smith <cierra.r.smith3@gmail.com>
2021-08-30 11:23:18 -0500

    added bonus from join exercises via walk through

--- join_exercises.sql
+++ join_exercises.sql
@@ -29,7 +29,8 @@ FROM employees as e
               ON e.emp_no = de.emp_no
          JOIN departments as d
               ON d.dept_no = de.dept_no
-WHERE de.to_date = '9999-01-01' AND d.dept_name = 'Customer Service'
+WHERE de.to_date = '9999-01-01' AND t.to_date = '9999-01-01'
+  AND d.dept_name = 'Customer Service'
 GROUP BY t.title
 ORDER BY 'Total' desc;
 
@@ -46,13 +47,22 @@ WHERE dm.to_date = '9999-01-01' AND s.to_date = '9999-01-01'
 ORDER BY d.dept_name;
 
 # Bonus Find the names of all current employees, their department name, and their current manager's name .
-# Continue working on this
-SELECT CONCAT(e.first_name, ' ', e.last_name) AS 'Employee', d.dept_name AS 'Department', CONCAT(e.first_name, ' ', e.last_name) AS 'Manager'
-FROM employees as e
+# Completed with walk through, need more help because this is confusing
+SELECT CONCAT(e.first_name, ' ', e.last_name) AS 'Employee', d.dept_name AS 'Department', CONCAT(e2.first_name, ' ', e2.last_name) AS 'Manager'
+FROM departments as d
+
+         JOIN dept_emp as de
+              ON d.dept_no = de.dept_no
          JOIN dept_manager as dm
-              ON dm.emp_no = e.emp_no
-         JOIN departments as d
               ON d.dept_no = dm.dept_no
-WHERE dm.to_date = '9999-01-01'
+         JOIN employees as e
+              ON de.emp_no = e.emp_no
+         JOIN employees as e2
+                ON dm.emp_no = e2.emp_no
+WHERE de.to_date = '9999-01-01' and dm.to_date = '9999-01-01'
 ORDER BY d.dept_name;
+
+
+
+
 
